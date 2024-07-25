23f55490c9e387f03eb99f38766da4516333110a
shahryar-karimi <shahryarkarimi20@gmail.com>
2024-07-26 01:27:25 +0330

    initial commit

---
+++ src/main/java/com/shy/glamspot/GlamSpotApplication.java
@@ -0,0 +1,13 @@
+package com.shy.glamspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlamSpotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlamSpotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shy/glamspot/GlamSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shy.glamspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlamSpotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
